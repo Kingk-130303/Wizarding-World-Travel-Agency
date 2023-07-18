e23433fdbb602e0c40331db48f5081b69f6de833
Kaival <mailkaivalshah@gmail.com>
2023-07-19 00:24:00 +0530

    Added more files related to auth

---
+++ Quill-and-Portkey/client/travel-agency/src/pages/js/adminpage.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import '../css/userpage.css';
+
+async function fetchData(SetIsLoggedIn) {
+  try {
+    const token = localStorage.getItem('jwtToken');
+    const response = await fetch('http://localhost:5000/api/private', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        token: token
+      }),
+    });
+    if (response.ok) {
+      SetIsLoggedIn(true);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const handleLogout = () => {
+   
+    localStorage.removeItem('jwtToken');
+    
+    window.location.href = '/login';
+  };
+
+function Adminpage() {
+  const [IsLoggedIn, SetIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    fetchData(SetIsLoggedIn);
+  }, []);
+
+  return (
+    <div>
+      {IsLoggedIn ? (
+        <><h1>Welcome to the admin's home page</h1>
+        <button onClick={handleLogout}>Logout</button></>
+        
+      ) : (
+        <h1>Invalid access, please login again</h1>
+      )}
+    </div>
+  );
+}
+
+export default Adminpage;
